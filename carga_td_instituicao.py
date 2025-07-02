ef6d5504466d399749475934f977f57fb7a9eb5a
Aureliano Boelter da Silva <aureliano.silva@ans.gov.br>
2025-07-02 17:56:33 -0300

    atualizado via cursor

--- carga_td_instituicao.py
+++ carga_td_instituicao.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # Dados em forma de dicionário
 dados = {
-    "Coluna1": ["Hosp_01", "Hosp_02", "Hosp_03"],
+    "Coluna1": ["Hosp_01", "Hosp_02", "Hosp_04"],
     "Coluna2": [25000, 320, 2910],
     "Coluna3": ["Espirito Santo", "Rio de Janeiro", "Belo Horizonte"]
 }
